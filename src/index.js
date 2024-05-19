6093b4d30176c036f17dcd83ce2f5424ace23ae4
JoySystem_v <fvv2011@gmail.com>
2024-05-19 20:06:40 +0300

    initial commit plus webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// src/index.js
+import './styles.css';
+
+// Ваш код JavaScript
+console.log('Hello, Webpack!');
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // Режим сборки
+  entry: './src/index.js', // Точка входа для сборки проекта
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/images', to: 'dist/images' },
+          { from: 'src/fonts', to: 'dist/fonts' },
+          { from: 'src/index.html', to: 'dist' },
+        ],
+      }),
+
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    open: true,
+    watchFiles: path.join(__dirname, 'src'),// Автоматически открывать браузер
+    port: 9000,
+     
+  },
+
+  
+};
